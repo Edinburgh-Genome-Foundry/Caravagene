539c1bdd0b03660272b3df0056e120dab2b15dde
Peter Vegh <pvegh@posteo.uk>
2020-09-23 13:27:33 +0100

    License fix

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('caravagene/version.py').read()) # loads __version__
+exec(open("caravagene/version.py").read())  # loads __version__
 
-setup(name='caravagene',
-      version=__version__,
-      author='Zulko',
-    description='',
-    long_description=open('README.rst').read(),
-    license='MIT',
+setup(
+    name="caravagene",
+    version=__version__,
+    author="Zulko",
+    description="",
+    long_description=open("README.rst").read(),
+    license="MIT",
     keywords="SBOL DNA assembly plot",
-    scripts=['scripts/caravagene'],
-    install_requires=['docopt', 'xlrd', 'pandas'],
+    scripts=["scripts/caravagene"],
+    install_requires=["docopt", "xlrd", "pandas"],
     include_package_data=True,
-    packages= find_packages(exclude='docs'))
+    packages=find_packages(exclude="docs"),
+)
